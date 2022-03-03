35544ca136fa94b656af9593b52ec21e41bd24f4
RinseWillet <r.willet@gmail.com>
2022-03-03 21:16:56 +0100

    minor changes

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import PokeCard from "./PokeCard";
 
 const App = () => {
 
-    const BASE_URL = "https://pokeapi.co/api/v2/pokemon/";
-
     return (
         <ul>
             <li><PokeCard></PokeCard></li>
